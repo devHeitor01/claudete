dc7ca7a4f39d8fdfa1743d38ea8938f9506e4535
devHeitor01 <heitor.pollastrini@aluno.ifsp.edu.br>
2025-05-15 10:01:37 -0300

    Add files via upload

---
+++ Aula15042025/primeiro.js
@@ -0,0 +1,29 @@
+//crianddo a função
+function validarNumero(){
+//criando variáveis
+    let numero = document.getElementById("numero").value
+    
+    let mensagem = documento.getElementById("mensagem")
+//desvio condicional encadeado/aninhado
+    if(numero === ""){
+        mensagem.textContent = "Por favor, insira um numero"
+        mensagem.style.color = "red"
+}else{
+    //converter texto para numero
+    numero= parseInt(numero)
+
+    if(numero > 10){
+        mensagem.textContent ="O número é maior que 10"
+        mensagem.style.color = "green"
+    }else{
+        if(numero > 5){
+            mensagem.textContent = "O número é maior que 5, mas menor quue 10"
+            mensagem.style.color = "orange"
+        }
+        else{
+            mensagem.textContent = "O número é 5 ou menor"
+            mensagem.style.color = "blue"
+        }
+    }
+}
+}
